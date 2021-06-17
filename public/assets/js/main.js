156f2d8b4f0624314dcb01ff5e1ccc4f6d5c3c24
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2021-06-17 21:38:55 +0300

    mesaj primul commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,12 @@
+function numeFilm(nume) {
+    b=document.getElementById('1').innerHTML;
+    console.log(nume.value);
+    window.location = "vizioneaza.html";
+    console.log(2);
+
+    function ceva() {
+        console.log('mege');
+        let d = document.getElementById('nume-film-test');
+    }
+
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { div } from 'prelude-ls';
+import React, { useState, useEffect,setState } from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Moovie />
+    </>
   );
+
 }
+function Moovie() {
+  var xhttp = new XMLHttpRequest();
+  var array = {
+    title: [],
+    about: [],
+    year: [],
+    urlPoster: [],
+    urlVideo: []
+  };
+  xhttp.onreadystatechange = function () {
+
+    if (this.readyState == 4 && this.status == 200) {
+      let i;
+      let xmlDoc = this.responseXML;
+      let x = xmlDoc.getElementsByTagName("MOVIE");
+      for (i = 0; i < x.length; i++) {
+
+        array.title.push(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+        array.about.push(x[i].getElementsByTagName("ABOUT")[0].childNodes[0].nodeValue);
+        array.year.push(x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
+        array.urlPoster.push(x[i].getElementsByTagName("URLPOSTER")[0].childNodes[0].nodeValue);
+
+
+      }
+
+
+    }
+ 
+
+  };
+  const [ar, setAr] = useState(array);
+  xhttp.open("GET", "xml.xml", true);
+  xhttp.send();
+  return ar.urlPoster.map((choice, i) => (
+    <div className="list-card" >
+      <a href="https://mealex.me">
+        <div className="list">
+
+          <img src={choice} alt="" ></img>
+        </div>
+        <div className="list-names">
+          <a href="vizioneaza.html" id="1" >{ar.title[i]}</a>
+          <button value="bright burn">haide </button>
+        </div>
+      </a>
+    </div >
+  ));
+}
+
+
+
 
 export default App;
